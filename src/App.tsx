b306e343872712b9dadde9e0c15af6c0bc6baefe
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-01-29 03:03:22 +0530

    feat: added reading type select dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,24 @@ import {GluestackUIProvider, Box} from '@gluestack-ui/themed';
 import AppTitle from './components/AppTitle';
 import {useTranslation} from 'react-i18next';
 import ReadingDate from './components/ReadingDate';
+import ReadingType from './components/ReadingType';
 
 function App(): JSX.Element {
   const {t} = useTranslation();
   const [date, setDate] = useState(new Date());
+  const [readingType, setReadingType] = useState('');
+
+  console.log('ReadingType = ', readingType);
   return (
     <SafeAreaView>
       <GluestackUIProvider config={config}>
-        <Box height="100%" py="$1" px="$2" bgColor="$emerald100">
+        <Box height="100%" py="$1" px="$3" bgColor="$emerald100">
           <AppTitle title={t('appTitle')} />
           <ReadingDate date={date} setDate={setDate} />
+          <ReadingType
+            readingType={readingType}
+            setReadingType={setReadingType}
+          />
         </Box>
       </GluestackUIProvider>
     </SafeAreaView>
