d721a18eb230256ff27d328dbc70c65f85be9121
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-01-29 02:37:47 +0530

    feat: added react native date picker

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 // eslint-disable-next-line no-unused-vars
 import i18n from './src/i18n';
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native';
 import {config} from '@gluestack-ui/config';
 import {GluestackUIProvider, Box} from '@gluestack-ui/themed';
-import AppTitle from './src/components/AppTitle';
+import AppTitle from './components/AppTitle';
 import {useTranslation} from 'react-i18next';
+import ReadingDate from './components/ReadingDate';
 
 function App(): JSX.Element {
   const {t} = useTranslation();
+  const [date, setDate] = useState(new Date());
   return (
     <SafeAreaView>
       <GluestackUIProvider config={config}>
         <Box height="100%" py="$1" px="$2" bgColor="$emerald100">
           <AppTitle title={t('appTitle')} />
+          <ReadingDate date={date} setDate={setDate} />
         </Box>
       </GluestackUIProvider>
     </SafeAreaView>
